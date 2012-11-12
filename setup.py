acdab4e0f19e7d28a51aeec67371e84cccb2b2a4
Michael.Imelfort <mike@mikeimelfort.com>
2012-11-12 23:18:50 +1000

    oops

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='SimpleHMMER',
-    version='0.0.1',
+    version='0.0.2',
     author='Michael Imelfort',
     author_email='mike@mikeimelfort.com',
     packages=['simplehmmer', 'simplehmmer.test'],
     scripts=[],
     url='http://pypi.python.org/pypi/SimpleHMMER/',
-    license='LICENSE.txt',
+    license='GPL3',
     description='SimpleHMMER',
     long_description=open('README.txt').read(),
     install_requires=[],
