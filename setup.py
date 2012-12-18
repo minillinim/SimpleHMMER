f45d2c82aab889cffa0ae207fdb759a381445a6c
Michael.Imelfort <mike@mikeimelfort.com>
2012-12-18 11:08:47 +1000

    version updates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='SimpleHMMER',
-    version='0.2.2',
+    version='0.2.3',
     author='Michael Imelfort',
     author_email='mike@mikeimelfort.com',
     packages=['simplehmmer', 'simplehmmer.test'],
